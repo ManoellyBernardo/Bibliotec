be93f5f77d62208fb037e8e21da7083fb4142698
ManoellyBernardo <manoellymaria@gmail.com>
2022-11-25 19:17:52 -0300

    Atualização

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
+import { NotificationService } from 'src/app/services/notification.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public formLogin: FormGroup;
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private notification: NotificationService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.formLogin = fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required]]
+    })
   }
 
+  ngOnInit(): void {
+    }
+
+  public signInGoogle(): void {
+      this.authService.authenticateByGoogle().subscribe(credencials => {
+        this.notification.showMessage("Bem-vindo(a)!");
+        this.router.navigate(["/home"]);
+      });
+    }
+
+  public signInEmailAndPassword(): void {
+      if(this.formLogin.valid) {
+      const user: User = this.formLogin.value;
+      this.authService.authenticateByEmailAndPassword(user).subscribe(credencials => {
+        this.notification.showMessage("Bem-vindo(a)!");
+        this.router.navigate(["/home"]);
+      });
+    }
+    else {
+      this.notification.showMessage("Dados inválidos.");
+    }
+  }
 }
